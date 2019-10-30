681d3ea260274c05cb720a48f97a00ec7e4e27d5
MarkoMile <mmarkomile@gmail.com>
2019-10-30 22:58:43 +0100

    Male promene, program se zapravo otvara sad

--- main.c
+++ main.c
@@ -2,24 +2,82 @@
 #include <stdlib.h>
 #include <string.h>
 
+
+
 int main()
 {
+  char check[3];
+  char name[18];
+  char location[20];
+  char command[20];
+
+  //---LEVEL 1---//             //Starter area
+  void s_house(char location[20])
+  {
+  printf("#SOUTH OF HOUSE#\n");
+  printf("You find yourself in a small valley, surrounded by mountains and a house to your north.\n");
+  printf("There is a sign.\n\n");
+  }
+
+
+  void n_house(char location[20])
+  {
+  location = "n_house";
+  printf("#NORTH OF HOUSE#\n");
+  printf("You are in a field, in front of the house.\n");
+  printf("There is a BIG HOLE to your north.\n\n");
+  printf("You see a door on the house.\n\n");
+  }
+
+
+  // COMMAND READING //
+  void readCommand(char command[20], char location[20])
+  {
+    scanf("%s", command);
+    if (strcmp(location, "s_house") == 0)             //location check
+    {
+      if (strcmp(command, "north") == 0)
+      {
+        location[20] = "n_house";
+        n_house(location);
+        readCommand(command,location);
+      }
+      //else if (strcmp(command[20], "walk east") == 0)
+      //{
+      //  e_house(location[20]);
+      //}
+      //else if (strcmp(command[20], "walk west") == 0)
+      //{
+      //  w_house(location[20]);
+      //}
+      else if (strcmp(command, "south") == 0)
+      {
+        printf("Can't walk south, the mountaints are too steep.");
+        location[20] = "s_house";
+        s_house(location);
+        readCommand(command,location);
+      }
+    }
+  }
+
+
+    //START//
+
     printf("Hello!\nWelcome to \"The Legend of The Fallen Warrior\"!\n\n");
-    char check[3];
-    char name[99];
+
 
     nazivanje:
       printf("What will be the name of your character\?\n");
 
-      scanf("%s", &name);
+      scanf("%s", name);
 
       printf("Are you sure you want your name to be %s?\n", name);
 
     provera:
       printf("Answer with \"YES\" ili \"NO\".\n");
 
-      scanf("%s", &check);
-      strupr(check);
+      scanf("%s", check);
+      //strupr(check);
 
       if(strcmp(check, "YES") == 0){                  //strcmp(str1,str2) proverava da li je svako slovo stringa 1 (zato sto je string niz karaktera) isto kao svako slovo stringa 2
           printf("Great!\n\n\n");                     //To radi tako sto proverava prvo slovo, zatim ako je prvo slovo isto onda proverava drugo, i tako redom.
@@ -29,60 +87,9 @@ int main()
           goto provera;
         }
 
-    // GAME PREP //
-    char location[20];
-    char command[20];
-
-
-    // COMMAND READING //
-    void readCommand(char command[20], char location[20])
-    {
-      scanf("%s", &command);
-      strlwr(command);
-
-      if (strcmp(location, "s_house") == 0)             //location check
-      {
-        if (strcmp(command, "walk north") == 0)
-        {
-          n_house(location);
-        }
-        //else if (strcmp(command[20], "walk east") == 0)
-        //{
-        //  e_house(location[20]);
-        //}
-        //else if (strcmp(command[20], "walk west") == 0)
-        //{
-        //  w_house(location[20]);
-        //}
-        else if (strcmp(command, "walk south") == 0)
-        {
-          printf("Can't walk south, the mountaints are too steep.");
-          s_house(location);
-        }
-      }
-    }
-
-    //---LEVEL 1---//             //Starter area
-    void s_house(char location[20]);
-    {
-      location[20] = "s_house";
-      printf("#SOUTH OF HOUSE#\n");
-      printf("You find yourself in a small valley, surrounded by mountains and a house to your north.\n");
-      printf("There is a sign.\n\n");
-
-      readCommand(command, location);
-    }
-
-
-    void n_house(char location[20]);
-      location[20] = "n_house";
-      printf("#NORTH OF HOUSE#\n");
-      printf("You are in a field, in front of the house.\n");
-      printf("There is a BIG HOLE to your north.\n\n");
-      printf("You see a door on the house.\n\n");
-
-      readCommand(command, location);
-
+        location[20] = "s_house";
+        s_house(location);
+        readCommand(command,location);
 
     return 0;
 }
